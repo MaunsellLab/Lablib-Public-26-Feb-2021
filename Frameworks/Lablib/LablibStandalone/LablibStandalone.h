e8caaff45ee56ec141d7402726ea74d506e4aa00
John Maunsell <maunsell@uchicago.edu>
2017-09-05 18:05:18 -0500

    Working on Freestanding Lablib build
    Signed-off-by: John Maunsell <maunsell@uchicago.edu>

---
+++ Frameworks/Lablib/LablibStandalone/LablibStandalone.h
@@ -0,0 +1,19 @@
+//
+//  LablibStandalone.h
+//  LablibStandalone
+//
+//  Created by John Maunsell on 9/4/17.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LablibStandalone.
+FOUNDATION_EXPORT double LablibStandaloneVersionNumber;
+
+//! Project version string for LablibStandalone.
+FOUNDATION_EXPORT const unsigned char LablibStandaloneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LablibStandalone/PublicHeader.h>
+
+
