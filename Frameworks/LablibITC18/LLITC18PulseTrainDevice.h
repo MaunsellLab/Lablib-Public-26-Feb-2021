8e58fcb0de6b3798772bbc09b2409ffb3bf75162
John Maunsell <maunsell@uchicago.edu>
2017-10-02 09:13:55 -0400

    LLITC18PulseTrainDevice
    Fixed definition of digital out event method.
    
    Signed-off-by: John Maunsell <maunsell@uchicago.edu>

--- Frameworks/LablibITC18/LLITC18PulseTrainDevice.h
+++ Frameworks/LablibITC18/LLITC18PulseTrainDevice.h
@@ -7,6 +7,7 @@
 //
 
 #import <Lablib/LLPulseTrainDevice.h>
+#import <Lablib/LLDataDevice.h>
 #import <ITC/Itcmm.h>
 
 @interface LLITC18PulseTrainDevice : NSObject <LLPulseTrainDevice>  {
@@ -28,10 +29,12 @@
 - (void)close;
 - (void)digitalOutputBits:(unsigned long)bits;
 - (void)digitalOutputBitsOff:(unsigned short)bits;
+- (void)digitalOutputBitsOn:(unsigned short)bits;
 - (void)doInitializationWithDevice:(long)numDevice;
 - (id)initWithDevice:(long)numDevice;
 - (id)initWithDataDevice:(LLDataDevice *)dataDevice;
 - (BOOL)makeInstructionsFromTrainData:(PulseTrainData *)pTrain channels:(long)channels;
 - (BOOL)open:(long)numDevice;
+- (BOOL)outputDigitalEvent:(long)event withData:(long)data;
 
 @end
